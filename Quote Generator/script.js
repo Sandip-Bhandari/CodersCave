24bd911a338084d1200ee1009c0c46e28f8f13f0
sandip <sandipbhandari805@gmail.com>
2023-07-31 20:57:39 +0530

    Quote Generator

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,39 +1,67 @@
-const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector("button"),
-authorName = document.querySelector(".name"),
-speechBtn = document.querySelector(".speech"),
-copyBtn = document.querySelector(".copy"),
-twitterBtn = document.querySelector(".twitter"),
-synth = speechSynthesis;
+document.addEventListener("DOMContentLoaded", () => {
+    const quoteText = document.querySelector(".quote");
+const authorName = document.querySelector(".name");
+const quoteBtn = document.querySelector("button");
+const speechBtn = document.querySelector(".speech");
+const copyBtn = document.querySelector(".copy");
 
-function randomQuote(){
-    quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
-    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
+const synth = window.speechSynthesis;
+
+function displayQuote(quote, author) {
+  quoteText.innerText = quote;
+  authorName.innerText = author;
+}
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+
+  fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => {
+      // Choose a random quote from the array of quotes
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const quote = data[randomIndex].text;
+      const author = data[randomIndex].author || "Unknown";
+      
+      displayQuote(quote, author);
+
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "Get Quote";
+    })
+    .catch(error => {
+      console.error('Error fetching the quote:', error);
+      quoteText.innerText = "Failed to fetch the quote. Please try again later.";
+      authorName.innerText = "Unknown";
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "Get Quote";
     });
 }
 
-speechBtn.addEventListener("click", ()=>{
-    if(!quoteBtn.classList.contains("loading")){
-        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
-        synth.speak(utterance);
-        setInterval(()=>{
-            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
-        }, 10);
-    }
-});
-
-copyBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(quoteText.innerText);
-});
-
-
-
 quoteBtn.addEventListener("click", randomQuote);
 
-  
-  
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    synth.speak(utterance);
+  }
+});
+
+copyBtn.addEventListener("click", () => {
+  const textToCopy = `${quoteText.innerText} - ${authorName.innerText}`;
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      alert("Quote copied to clipboard!");
+    })
+    .catch(error => {
+      console.error('Error copying to clipboard:', error);
+      alert("Failed to copy the quote. Please try again.");
+    });
+});
+
+// Load initial quote on page load
+randomQuote();
+
+
+  });
+
